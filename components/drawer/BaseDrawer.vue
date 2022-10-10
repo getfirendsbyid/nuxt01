22fe245916c2909acce1561341464405d8493017
getfirendsbyid <1094692718@qq.com>
2022-10-10 20:49:44 +0800

    z

--- components/drawer/BaseDrawer.vue
+++ components/drawer/BaseDrawer.vue
@@ -1,36 +1,35 @@
 <!-- eslint-disable vue/no-multiple-template-root -->
 <template>
-
-  <n-drawer v-model:show="active" :width="200" :placement="placement" close-on-esc >
+  <n-drawer v-model:show="active" :width="200" :placement="placement" close-on-esc>
     <n-drawer-content title="关闭" closable>
       <NuxtLink v-for="(item,index) in navigation" :key="index" :class="[item.current ? 'bg-gray-900 text-black' : 'text-gray-700 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :to="item.href">
         {{ item.name }}
       </NuxtLink>
-     
     </n-drawer-content>
   </n-drawer>
 </template>
 
 <script lang="ts">
-import { ref ,defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue'
 import type { DrawerPlacement } from 'naive-ui'
 
 export default defineComponent({
-  props:{
-  active: Boolean,
-  navigation: Array
+  props: {
+    active: Boolean,
+    navigation: Array
   },
-  setup (props,context) {
-      const active = ref(props.active)
-      const placement = ref<DrawerPlacement>('left')
-      console.log(props,'子组件')
-      return {
-        active: active,
-        placement: placement,
-      }
-}
+  setup (props, context) {
+    const active = ref(props.active)
+    const navigation = ref(props.navigation)
+    const placement = ref<DrawerPlacement>('left')
+    console.log(props, '子组件')
+    return {
+      active,
+      navigation,
+      placement
+    }
+  }
 
 })
-
 
 </script>
