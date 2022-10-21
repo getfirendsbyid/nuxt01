453add254c36ae92563c71f43642e86842e4f37f
georgeyv <8842869+georgeyv@user.noreply.gitee.com>
2022-10-21 15:12:40 +0800

    'z'

--- components/imgList/heroList.vue
+++ components/imgList/heroList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 xl:gap-x-6">
     <div v-for="(item,index) in datas" :key="index">
-      <NuxtLink :to="'/lol/hero_'+item.id+'/1'" :alt="item.name +','+ item.alias +','+ item.title">
+      <NuxtLink :to="'/lol/hero_'+item.id+'/skin_0/1'" :alt="item.name +','+ item.alias +','+ item.title">
         <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
           <img :src="item.imgUrl" :alt="item.name +','+ item.alias +','+ item.title" class="h-full w-full object-cover object-center group-hover:opacity-75">
         </div>
